4c03933a6a20f0242668fc9d08a9d3a1b79acfae
LyubenSB <lsbworkedu@gmail.com>
2018-04-24 12:38:33 +0300

    new demo init commit

---
+++ BookSpace/BookSpace/src/BookSpace.Data/BookSpaceContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookSpace.Data.Contracts;
+using BookSpace.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookSpace.Data
+{
+    public class BookSpaceContext : IdentityDbContext<User>, IDbContext 
+    {
+        public BookSpaceContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       // public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Add configurations
+
+            //builder.ApplyConfiguration();
+        }
+
+    }
+}
